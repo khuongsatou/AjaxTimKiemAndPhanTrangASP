aeb5e146bf7e00a62f6abe10733f4a408b5115c2
khuongsatou <yoshi240499@gmail.com>
2019-12-20 23:02:44 +0700

    version 1.2

---
+++ Ktra/KiemTra/KtraASPX/DataProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace KtraASPX
+{
+    public class DataProvider
+    {
+        private static SqlConnection conn = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=dbKTra;Integrated Security=True");
+
+        public static void connect()
+        {
+            if (conn.State == ConnectionState.Closed || conn.State == ConnectionState.Broken)
+            {
+                conn.Open();
+            }
+        }
+
+        //Có Trả về 1 bảng -> Select
+        public static DataTable getDataTable(string sQuery, SqlParameter[] param)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                connect();
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                cmd.Connection = conn;
+                cmd.CommandText = sQuery;
+                cmd.Parameters.AddRange(param);
+                adapter.SelectCommand = cmd;
+
+                adapter.Fill(dt);
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            return dt;
+        }
+    }
+}
---
+++ Ktra/KiemTra/KtraASPX/SanPham.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace KtraASPX
+{
+    public class SanPham
+    {
+        private int _id;
+        private string _tenSP;
+        private string _hinhAnh;
+        private double _gia;
+
+        public int Id { get => _id; set => _id = value; }
+        public string TenSP { get => _tenSP; set => _tenSP = value; }
+        public string HinhAnh { get => _hinhAnh; set => _hinhAnh = value; }
+        public double Gia { get => _gia; set => _gia = value; }
+
+        public SanPham()
+        {
+            _id = 0;
+            _tenSP = "";
+            _hinhAnh = "";
+            _gia = 0;
+        }
+
+        public SanPham(int id, string tenSP, string hinhAnh, double gia)
+        {
+            _id = id;
+            _tenSP = tenSP;
+            _hinhAnh = hinhAnh;
+            _gia = gia;
+        }
+
+        public List<SanPham> getList(string sQuery,SqlParameter[] param)
+        {
+            List<SanPham> sp = new List<SanPham>();
+            DataTable dt = DataProvider.getDataTable(sQuery, param);
+            foreach (DataRow dr in dt.Rows)
+            {
+                sp.Add(convertToObject(dr));
+            }
+            return sp;
+        }
+
+        private SanPham convertToObject(DataRow dr)
+        {
+            SanPham sp = new SanPham();
+            sp.Id = Convert.ToInt32(dr["ID"]);
+            sp.TenSP = dr["TenSP"].ToString();
+            sp.HinhAnh = dr["HinhAnh"].ToString();
+            sp.Gia =Convert.ToDouble(dr["Gia"].ToString());
+            return sp;
+        }
+
+    }
+}
